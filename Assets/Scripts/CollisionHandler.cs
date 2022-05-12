e17e1933e02e17917676f0feaea1639c8db9f91b
Dipesh1jain <85340142+Dipesh1jain@users.noreply.github.com>
2022-05-13 01:46:41 +0530

    Triggered Player Explosion
    till lec 92

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem ExplosionVFX;
+    [SerializeField] GameObject[] fighterParts;
     // Start is called before the first frame update
-    void OnCollisionEnter(Collision other)
-    {
-        Debug.Log(this.name + " --collided with--" + other.gameObject.name);
-       
-    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"{this.name} **Triggered by {other.name}"); // string interpolation method to print a messeage if concatination is need ,
                                                                // methodname within '{}' start with dollar sign
+       
+        StartCrashSequence();
+    }
+
+     void StartCrashSequence()
+    {
+        GetComponent<PlayerControls>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+        ExplosionVFX.Play();
+        TurnOffParts();
+        
+    }
+    void TurnOffParts()
+
+    {
+
+        int i = 0;
+
+        for (i = 0; i < fighterParts.Length; ++i)
+
+        {
+
+            fighterParts[i].GetComponent<MeshRenderer>().enabled = false;
+
+        }
+
+    }
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
