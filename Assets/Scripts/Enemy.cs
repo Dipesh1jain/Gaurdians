85ff4e0e4f7d1b099f042eb01527e227d23c776e
Dipesh1jain <85340142+Dipesh1jain@users.noreply.github.com>
2022-05-13 12:40:13 +0530

    Instantiating explosion at runtime
    till lec 93

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
     // Start is called before the first frame update
     void OnParticleCollision(GameObject other)
     {
-        Debug.Log($"{name} i am hit by  {other.gameObject.name }");
+
+       GameObject vfx= Instantiate(deathVFX, transform.position, Quaternion.identity); //using this Instantiate method so that we can call the deathvfx at runtime
+        vfx.transform.parent = parent;                                                              //rater than making it a part of enemy prefab
         Destroy(gameObject);
     }
 }
